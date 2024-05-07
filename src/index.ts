b59078e2107d86c8c1e8d634659db8f39677a3bb
Marlon Divino <marlon@yotha.com.br>
2024-05-07 20:48:53 -0300

    Versão inicial

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-import express, { Application } from "express";
-import morgan from "morgan";
-import swaggerUi from "swagger-ui-express";
-import Router from "./routes";
+import { AppDataSource } from "./data-source";
+import * as express from "express";
+import * as dotenv from "dotenv";
+import { Request, Response } from "express";
+import { userRouter } from "./routers/user.routes";
+import { movieRouter } from "./routers/movie.routes";
+import "reflect-metadata";
+import { errorHandler } from "./middlewares/error.middleware"
+dotenv.config();
 
-const PORT = process.env.PORT || 8000;
-
-const app: Application = express();
+const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
-app.use(express.static("public"));
-app.use(
-    "/docs",
-    swaggerUi.serve,
-    swaggerUi.setup(undefined, {
-        swaggerOptions: {
-            url: "/swagger.json",
-        },
-    })
-);
-app.use(Router);
+app.use(errorHandler);
+const { PORT = 3000 } = process.env;
+app.use("/auth", userRouter);
+app.use("/api", movieRouter);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
-    console.log(`Swagger: http://localhost:${PORT}/docs/`)
+app.get("*", (req: Request, res: Response) => {
+    res.status(505).json({ message: "Bad Request" });
 });
+
+AppDataSource.initialize()
+    .then(async () => {
+        app.listen(PORT, () => {
+            console.log("Server is running on http://localhost:" + PORT);
+        });
+        console.log("Data Source has been initialized!");
+    })
+    .catch((error) => console.log(error));
---
+++ src/routers/user.routes.ts
@@ -0,0 +1,33 @@
+import * as express from "express";
+import { authentification, authorization } from "../middlewares/auth.middleware";
+import { UserController } from "../controllers/user.controllers";
+import { AuthController } from "../controllers/auth.controller";
+const Router = express.Router();
+
+Router.get(
+    "/users",
+    authentification,
+    authorization(["admin"]),
+    UserController.getUsers
+);
+Router.get(
+    "/profile",
+    authentification,
+    authorization(["user", "admin"]),
+    AuthController.getProfile
+);
+Router.post("/signup", UserController.signup);
+Router.post("/login", AuthController.login);
+Router.put(
+    "/update/:id",
+    authentification,
+    authorization(["user", "admin"]),
+    UserController.updateUser
+);
+Router.delete(
+    "/delete/:id",
+    authentification,
+    authorization(["admin"]),
+    UserController.deleteUser
+);
+export { Router as userRouter };
